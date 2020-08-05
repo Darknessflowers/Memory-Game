e03d34d5a1160c86a6609282e4118240fb9c72c4
Darknessflowers <darknessflowers@hotmail.co.uk>
2020-08-05 15:34:09 +1200

    Changes

--- script.js
+++ script.js
@@ -14,102 +14,82 @@ let pairs = [
   {
     id: 1,
     frontFace: 'img/jonti-blanket.jpg',
-    found: false,
   },
   {
     id: 2 ,
     frontFace: 'img/jonti-blanket.jpg',
-    found: false,
   },
   {
     id: 3 ,
     frontFace: 'img/jonti-couch.jpg',
-    found: false,
   },
   {
     id: 4 ,
     frontFace: 'img/jonti-couch.jpg',
-    found: false,
   },
   {
     id: 5 ,
     frontFace: 'img/jonti-grass.jpg',
-    found: false,
   },
   {
     id: 6 ,
     frontFace: 'img/jonti-grass.jpg',
-    found: false,
   },
   {
     id: 7 ,
     frontFace: 'img/jonti-happy.jpg',
-    found: false,
   },
   {
     id: 8 ,
     frontFace: 'img/jonti-happy.jpg',
-    found: false,
   },
   {
     id: 9 ,
     frontFace: 'img/jonti-roar.jpg',
-    found: false,
   },
   {
     id: 10 ,
     frontFace: 'img/jonti-roar.jpg',
-    found: false,
   },
   {
     id: 11 ,
     frontFace: 'img/jonti-smile.jpg',
-    found: false,
   },
   {
     id: 12 ,
     frontFace: 'img/jonti-smile.jpg',
-    found: false,
   },
   {
     id: 13 ,
     frontFace: 'img/jonti-stern.jpg',
-    found: false,
   },
   {
     id: 14 ,
     frontFace: 'img/jonti-stern.jpg',
-    found: false,
   },
   {
     id: 15 ,
     frontFace: 'img/jonti-tongue.jpg',
-    found: false,
   },
   {
     id: 16 ,
     frontFace: 'img/jonti-tongue.jpg',
-    found: false,
   },
   {
     id: 17 ,
     frontFace: 'img/jonti-upsidedown.jpg',
-    found: false,
   },
   {
     id: 18 ,
     frontFace: 'img/jonti-upsidedown.jpg',
-    found: false,
   },
   {
     id: 19 ,
     frontFace: 'img/jonti-wtf.jpg',
-    found: false,
   },
   {
     id: 20 ,
     frontFace: 'img/jonti-wtf.jpg',
-    found: false,
   },
 ];
 
@@ -133,22 +113,28 @@ function flipCard(e) {
 
   if(!hasBeenFlipped) {
     firstCard = this;
-
     hasBeenFlipped = true;
-  } else {
-    secondCard = this;
-    hasBeenFlipped = false;
-    // return;
+  } 
+  else {
+    if(this.lastElementChild.id !== firstCard.firstElementChild.id) {
+        secondCard = this;
+    }
+    else {
+      hasBeenFlipped = false;
+      // return;
+    }
   }
   this.classList.add('flip');
+    //remove hidden from Frontface
   this.lastElementChild.classList.remove('hidden');
+    //add hidden to backface
   this.firstElementChild.classList.add('hidden');
 
   cardsTurned.push(this.lastElementChild.src);
   // console.log(cardsTurned);
   if(cardsTurned.length === 2) {
     console.log('2 turned');
-    if(firstCard.lastElementChild.src === secondCard.lastElementChild.src) {
+    if(firstCard.lastElementChild.src === secondCard.lastElementChild.src && firstCard.lastElementChild.id !== secondCard.lastElementChild.id ) {
       increaseScore();
       cardsTurned = [];
       firstCard.classList.add('permaFlip');
@@ -174,6 +160,7 @@ function addImage() {
   //loop through array of cards and attach image from corresponding index of shuffled array
   cards.forEach((card,index) => { 
     card.lastElementChild.src = shuffledArray[index].frontFace;
+    card.lastElementChild.id = shuffledArray[index].id;
   });
 }
 function increaseScore() {
